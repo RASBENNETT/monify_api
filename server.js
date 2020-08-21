39d303b8d85c4000a6cc2a06af43cb5efcdcd06f
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-08-21 20:22:40 +0100

    add barebones properties route

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 let port = process.env.PORT || 3001
 
+const properties = require("./json_store/Properties.json")
+
 app.get("/houses/", (req, res) => {
-    res.send("Hello World")
+    res.send(properties)
 })
 
 app.listen(port, () => {

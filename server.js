f5e24dfcf0db7b8da57aec7f3ca95443bc7ae7b7
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-08-25 17:49:36 +0100

    add query params to routes

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ let port = process.env.PORT || 3001
 
 const properties = require("./json_store/Properties.json")
 
-app.get("/houses/", (req, res) => {
-    res.send(properties)
+
+app.get("/houses", (req, res) => {
+
+    if (req.query.price) {
+        if (req.query.price == "decreasing") {res.send(properties.sort((a,b) => {return b.price - a.price}))}
+        if (req.query.price == "increasing") {res.send(properties.sort((a,b) => {return a.price - b.price}))}
+        
+        res.send(properties.filter(property => property.price == req.query.price))
+    }
+    else {
+        res.send(properties)
+    }
+
 })
 
 app.listen(port, () => {

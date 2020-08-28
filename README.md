68e8bd755ee6877d79e1befbdc1e477eaed9d2c0
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-08-28 01:16:28 +0100

    add store route for all products

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+# Monify Api
 
+## Development
 
+To run in development environment use 
+
+``` npm run server ```
+
+development routes have http://localhost:3001/ prefix
+
+---
+## Production - Live Routes
+
+### All
+
+https://monifyapi.herokuapp.com/store -> Returns all items in store
+
+### Category
+
+https://monifyapi.herokuapp.com/store/<< Category >>
+
+e.g.
+
+ https://monifyapi.herokuapp.com/store/fashion -> returns all items in the fashion category
+
+### Price Filter Category
+
+https://monifyapi.herokuapp.com/store/fashion?price=<< Filter >>
+
+Filter can be either : A specific <b> price </b> (£6500); <b> increasing </b> (low to high); <b> decreasing </b> (high to low)
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ let port = process.env.PORT || 3001
 
 const store = require("./json_store/store.json")
 
+app.get("/store", (req, res) => {
+    res.send(store)
+})
+
 app.get("/store/properties", (req, res) => {
     priceFilter(res, req.query, store.properties)
 })
@@ -12,20 +16,16 @@ app.get("/store/fashion", (req, res) => {
     priceFilter(res, req.query, store.fashion)
 })
 
-
 const priceFilter = (res, query, array) => {
     if (query && query.price) {
-        
         if (query.price == "decreasing") {res.send(array.sort((a,b) => {return b.price - a.price}))}
         if (query.price == "increasing") {res.send(array.sort((a,b) => {return a.price - b.price}))}
-        
         res.send(array.filter(product => product.price == query.price))
     }
     else {
         res.send(array)
     }
 }
-
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
